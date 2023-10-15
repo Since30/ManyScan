70d9cd1812e94cefa1e336427f953f4fb4607efb
antoinedesp <114112600+antoinedesp@users.noreply.github.com>
2023-10-15 11:20:30 +0200

    Components: Header
    Todo:
    - Rendre  responsive
    - Ajouter logo

---
+++ src/app/components/header.tsx
@@ -0,0 +1,17 @@
+'use client' // Nécéssaire pour pouvoir utiliser onChangeHandler
+
+import Toggle from './toggle'
+
+export default function Header() {
+    return (
+        <header className="bg-black px-4 py-6 flex justify-between">
+            <h2 className="font-bold text-white text-2xl">ManyScan</h2>                
+            <Toggle 
+                value={false} 
+                onChangeHandler= { (value: boolean) =>
+                    console.log(value) // TODO: Sauvegarder ici dans les préférences le choix de l'utilisateur
+                }
+                label="Dark mode"></Toggle> 
+        </header>
+    )
+}
