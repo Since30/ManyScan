f39a6f7be5e6d5bf62284038b2686a2774d649d1
Saflou <dz-siross@hotmail.fr>
2023-11-24 21:28:29 +0100

    add footer transition

--- client/app/components/footer.tsx
+++ client/app/components/footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className="bg-black text-white h-20 flex justify-end items-center p-4">
+    <div className="transition flex justify-between py-4 items-center font-bold bg-light-card dark:bg-dark-card px-20 m-auto">
       {/* h-20 est approximativement égal à 70px */}
       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         Nous Contacter
