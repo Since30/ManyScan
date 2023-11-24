ba07adb6f02d969ee3871240d678180bfbc7107a
Saflou <dz-siross@hotmail.fr>
2023-11-24 20:03:26 +0100

    add plugin tailwind scrollbar & pages dashboard

--- server/index.js
+++ server/index.js
@@ -1,34 +1,38 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const sequelize = require("./config/db");
 
-const mangasRoutes = require('./routes/mangas.routes');
-const usersRoutes = require('./routes/users.routes');
-const authRouter = require('./routes/authentication.router');
+const mangasRoutes = require("./routes/mangas.routes");
+const usersRoutes = require("./routes/users.routes");
+const authRouter = require("./routes/authentication.router");
 
-require('dotenv').config({ path: './config/.env' });
-require('./config/db')
+require("dotenv").config({ path: "./config/.env" });
+require("./config/db");
 
 const app = express();
 app.use(bodyParser.json());
-
 
 //----- Headers & autorizations -----//
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-sequelize.sync(
+sequelize
+  .sync
   // {force: true}
-).then(()=>console.log('database is ready'));
+  ()
+  .then(() => console.log("database is ready"));
+
+app.get("/", (req, res) => {
+  res.send("Bienvenue sur l'api ManyManga!");
+});
 
 // Middlewares
 
 // Routes
-app.use('/api/mangas', mangasRoutes);
-app.use('/api/users', usersRoutes);
-app.use('/api/auth', authRouter);
-
+app.use("/api/mangas", mangasRoutes);
+app.use("/api/users", usersRoutes);
+app.use("/api/auth", authRouter);
 
 module.exports = app;
