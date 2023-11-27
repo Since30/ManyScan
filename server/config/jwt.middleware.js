a03f60891011fe56fc39617d5cc30f74b063e7dc
Bastardrosset <thexav01@outlook.fr>
2023-11-27 17:07:56 +0100

    Ajout de sanitize && middleware sanitize aux routes register & login, fonctionne

---
+++ server/config/jwt.middleware.js
@@ -0,0 +1,26 @@
+const JWT = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+    try{
+        let userToken = req.headers.authorization.split(' ')[1]; // Check token dans les entetes
+        
+        if(userToken){
+            JWT.verify(userToken, process.env.SECRET_TOKEN_KEY, {
+            }, (error, data) => {
+                if(error){
+                    return res.status(500).json({
+                        message: "token is not valid",
+                        error: error
+                    })
+                } else{
+                    req.data = data;
+                    next();
+                }
+            });
+        } else{
+        return res.status(500).json({ message: 'Authentication token value failed' });
+        }
+    } catch(error){
+        res.status(500).json({ message: 'The token value is invalid', error: error});
+    }
+}
