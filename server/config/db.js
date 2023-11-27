f887486aa1dbb39f12790369403a39c68dbe5944
Bastardrosset <thexav01@outlook.fr>
2023-11-27 18:38:33 +0100

    Ajout auth controller & sanitize

---
+++ server/config/db.js
@@ -0,0 +1,13 @@
+const { Sequelize } = require("sequelize");
+const path = require('path');
+
+// Spécifiez le chemin complet du fichier SQLite
+const databasePath = path.join(__dirname, './database.sqlite');
+
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    host: databasePath,
+});
+
+module.exports = sequelize;
+
