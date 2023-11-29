7e5f96b775d696260cd9b420117022045dfbe745
Saflou <dz-siross@hotmail.fr>
2023-11-29 15:45:15 +0100

    maj mangadetail and add reading manga

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,7 +1,6 @@
 'use client';
 import React, { useState, useEffect } from 'react';
 import Header from '../components/header';
-import Navigation from '../components/navigation';
 import TopAnimeCard from '../cards/TopAnimeCard';
 import DiscoverAnimeCard from '../cards/DiscoverAnimeCard';
 import NewAnimeCard from '../cards/NewAnimeCard';
@@ -9,6 +8,8 @@ import TopVerticalAnimeCard from '../cards/TopVerticalAnimeCard';
 import { fetchManga } from '../services/MangaTheqApi';
 import AnimeObjects from '../interfaces/animeObjects';
 import Footer from '../components/footer';
+import AllMangas from '@/components/allmangas';
+import Contact from '@/components/contact';
 
 export default function Home() {
   const [animes, setAnimes] = useState<AnimeObjects[]>([]);
@@ -26,18 +27,18 @@ export default function Home() {
   return (
     <div className='py-6'>
       <Header />
-      <Navigation />
+     
       <div className="lg:flex">
         <div className="lg:w-3/4">
           <div className="lg:flex">
             {animes.length > 0 && (
-              <div className="w-full lg:w-3/4">
+              <div className="w-full lg:w-3/4 mt-12">
               
                 <TopAnimeCard animes={animes[0]} />
               </div>
             )}
             {animes.length > 1 && (
-              <div className="w-full lg:w-1/4">
+              <div className="w-full lg:w-1/4 mt-12">
                 
                 <DiscoverAnimeCard anime={animes[1]} />
               </div>
@@ -47,11 +48,15 @@ export default function Home() {
             <NewAnimeCard animes={animes} />
           </div>
         </div>
-        <div className="lg:w-1/4">
+        <div className="lg:w-1/4 mt-12">
       
           <TopVerticalAnimeCard animes={animes} title="Top Vertical" />
         </div>
       </div>
+      <main>
+        <AllMangas />
+      </main>
+      <Contact />
       <Footer />
     </div>
   );
---
+++ client/src/pages/read/[chapterId].tsx
@@ -0,0 +1,51 @@
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+
+const ChapterReaderPage = () => {
+  const router = useRouter();
+  const { chapterId } = router.query;
+  const [chapterImages, setChapterImages] = useState<string[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchChapterImages = async () => {
+      if (typeof chapterId === 'string') {
+        try {
+          const response = await fetch(`https://api.mangadex.org/at-home/server/${chapterId}`);
+          const data = await response.json();
+
+          if (data.result === 'ok') {
+            const baseUrl = data.baseUrl;
+            const chapterHash = data.chapter.hash;
+            const pageFilenames = data.chapter.data; // ou data.chapter.dataSaver pour la qualité compressée
+
+            // Déclarez explicitement le type du paramètre 'filename' ici
+            const imageUrls = pageFilenames.map((filename: string) => `${baseUrl}/data/${chapterHash}/${filename}`);
+            setChapterImages(imageUrls);
+          }
+        } catch (error) {
+          console.error('Error fetching chapter images:', error);
+        } finally {
+          setIsLoading(false);
+        }
+      }
+    };
+
+    fetchChapterImages();
+  }, [chapterId]);
+
+  if (isLoading) {
+    return <div>Chargement du chapitre...</div>;
+  }
+
+  return (
+    <div>
+      {chapterImages.map((imageUrl, index) => (
+        <img key={index} src={imageUrl} alt={`Page ${index + 1}`} className="w-full" />
+      ))}
+    </div>
+  );
+};
+
+export default ChapterReaderPage;
+
