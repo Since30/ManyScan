a464c702d463a4814172dc05e81dfd6671a7fa9f
Saflou <dz-siross@hotmail.fr>
2023-12-05 20:23:26 +0100

    add function register to server

--- server/mangas-api.js
+++ server/mangas-api.js
@@ -1,4 +1,4 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
 //Récupération de la cover
 const getCover = async (manga) => {
@@ -20,13 +20,16 @@ const getStatistics = async (manga) => {
   const rating = await data.statistics[manga.id].rating.average;
   const numberOfVotes = await data.statistics[manga.id].rating.distribution;
 
-    const totalVotes = Object.values(numberOfVotes).reduce((sum, value) => sum + value, 0);
+  const totalVotes = Object.values(numberOfVotes).reduce(
+    (sum, value) => sum + value,
+    0
+  );
 
-    const statistics = {
-        rating: rating,
-        numberOfVotes: totalVotes,
-    };
-    return statistics ;
+  const statistics = {
+    rating: rating,
+    numberOfVotes: totalVotes,
+  };
+  return statistics;
 };
 
 // Retourne un array de 20 mangas max
@@ -58,124 +61,124 @@ module.exports.searchByTitle = async (title) => {
 //L'offset permet un affichage 20 par 20 en spécifiant un numéro de page dans la requête
 
 module.exports.getAllMangas = async (page = 1) => {
+  const baseUrl = new URL("https://api.mangadex.org/manga");
 
-    const baseUrl = new URL('https://api.mangadex.org/manga');
+  const addParams = (params) => {
+    // Ajouter les paramètres à l'URL
+    Object.keys(params).forEach((key) => {
+      if (Array.isArray(params[key])) {
+        params[key].forEach((value) => baseUrl.searchParams.append(key, value));
+      } else {
+        baseUrl.searchParams.append(key, params[key]);
+      }
+    });
+  };
 
-    const addParams = (params) => {
-        // Ajouter les paramètres à l'URL
-        Object.keys(params).forEach((key) => {
-            if (Array.isArray(params[key])) {
-                params[key].forEach((value) =>
-                    baseUrl.searchParams.append(key, value)
-                );
-            } else {
-                baseUrl.searchParams.append(key, params[key]);
-            }
-        });
-    };
-    
-   const params = {
-        'contentRating[]': 'safe',
-        'includes[]': ['cover_art', 'author'],
-        'availableTranslatedLanguage[]': 'fr',
-        limit: 20,
-        offset: page * 20,
-    };
-    
-    addParams(params)
+  const params = {
+    "contentRating[]": "safe",
+    "includes[]": ["cover_art", "author"],
+    "availableTranslatedLanguage[]": "fr",
+    limit: 20,
+    offset: page * 20,
+  };
+
+  addParams(params);
   try {
     // Effectuer la requête GET
     const response = await fetch(baseUrl);
 
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const data = await response.json();
-        // Création d'un tableau d'objets manga
-
-        const mangasPromises = data.data.map(async (manga) => {
-            const coverURL = await getCover(manga);
-            const statistics = await getStatistics(manga);
-
-            const mangaObj = {
-                id: manga.id,
-                title: manga.attributes.title.en,
-                description: manga.attributes.description.en,
-                type: manga.attributes.publicationDemographic,
-                status: manga.attributes.status,
-                year: manga.attributes.year,
-                createAt: manga.attributes.createdAt,
-                updatedAt: manga.attributes.updatedAt,
-                language:
-                    manga.attributes.availableTranslatedLanguages.join(' '),
-                lastChapter: manga.attributes.latestUploadedChapter,
-                statistics: statistics,
-                //coverFileName: manga.relationships.find((relationship) => relationship.type === 'cover_art').attributes.fileName,
-                cover: coverURL,
-                authorId: manga.relationships.find(
-                    (relationship) => relationship.type === 'author'
-                ).id,
-                authorName: manga.relationships.find(
-                    (relationship) => relationship.type === 'author'
-                ).attributes.name,
-            };
-            return mangaObj;
-        });
-
-        // Attendre que toutes les promesses soient résolues
-        const mangas = await Promise.all(mangasPromises);
-
-        return mangas;
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        throw error;
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
+    const data = await response.json();
+    // Création d'un tableau d'objets manga
+
+    const mangasPromises = data.data.map(async (manga) => {
+      const coverURL = await getCover(manga);
+      const statistics = await getStatistics(manga);
+
+      const mangaObj = {
+        id: manga.id,
+        title: manga.attributes.title.en,
+        description: manga.attributes.description.en,
+        type: manga.attributes.publicationDemographic,
+        status: manga.attributes.status,
+        year: manga.attributes.year,
+        createAt: manga.attributes.createdAt,
+        updatedAt: manga.attributes.updatedAt,
+        language: manga.attributes.availableTranslatedLanguages.join(" "),
+        lastChapter: manga.attributes.latestUploadedChapter,
+        statistics: statistics,
+        //coverFileName: manga.relationships.find((relationship) => relationship.type === 'cover_art').attributes.fileName,
+        cover: coverURL,
+        authorId: manga.relationships.find(
+          (relationship) => relationship.type === "author"
+        ).id,
+        authorName: manga.relationships.find(
+          (relationship) => relationship.type === "author"
+        ).attributes.name,
+      };
+      return mangaObj;
+    });
+
+    // Attendre que toutes les promesses soient résolues
+    const mangas = await Promise.all(mangasPromises);
+
+    return mangas;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
 };
 
-
 module.exports.getFavorites = async (favorites) => {
-    try {
-        // Transformer chaque favorite manga en une requête de fetch individuelle
-        const favoriteMangasPromises = await favorites.map(async (favorite) => {
+  try {
+    // Transformer chaque favorite manga en une requête de fetch individuelle
+    const favoriteMangasPromises = await favorites.map(async (favorite) => {
+      const response = await fetch(
+        `https://api.mangadex.org/manga/${favorite.mangaId}?includes[]=cover_art&includes[]=author`
+      );
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
 
-            const response = await fetch(`https://api.mangadex.org/manga/${favorite.mangaId}?includes[]=cover_art&includes[]=author`);
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
+      const data = await response.json();
+      const manga = data.data;
 
-            const data = await response.json();
-            const manga = data.data;
+      // Construire l'objet manga
+      const coverURL = await getCover(manga);
+      const statistics = await getStatistics(manga);
 
-            // Construire l'objet manga
-            const coverURL = await getCover(manga);
-            const statistics = await getStatistics(manga);
+      const mangaObj = {
+        id: manga.id,
+        title: manga.attributes.title.en,
+        description: manga.attributes.description.en,
+        type: manga.attributes.publicationDemographic,
+        status: manga.attributes.status,
+        year: manga.attributes.year,
+        createAt: manga.attributes.createdAt,
+        updatedAt: manga.attributes.updatedAt,
+        language: manga.attributes.availableTranslatedLanguages.join(" "),
+        lastChapter: manga.attributes.latestUploadedChapter,
+        statistics: statistics,
+        cover: coverURL,
+        authorId: manga.relationships.find(
+          (relationship) => relationship.type === "author"
+        ).id,
+        authorName: manga.relationships.find(
+          (relationship) => relationship.type === "author"
+        ).attributes.name,
+      };
 
-            const mangaObj = {
-                id: manga.id,
-                title: manga.attributes.title.en,
-                description: manga.attributes.description.en,
-                type: manga.attributes.publicationDemographic,
-                status: manga.attributes.status,
-                year: manga.attributes.year,
-                createAt: manga.attributes.createdAt,
-                updatedAt: manga.attributes.updatedAt,
-                language: manga.attributes.availableTranslatedLanguages.join(' '),
-                lastChapter: manga.attributes.latestUploadedChapter,
-                statistics: statistics,
-                cover: coverURL,
-                authorId: manga.relationships.find((relationship) => relationship.type === 'author').id,
-                authorName: manga.relationships.find((relationship) => relationship.type === 'author').attributes.name,
-            };
+      return mangaObj;
+    });
 
-            return mangaObj;
-        });
+    // Attendre que toutes les requêtes individuelles soient résolues
+    const favoriteMangas = await Promise.all(favoriteMangasPromises);
 
-        // Attendre que toutes les requêtes individuelles soient résolues
-        const favoriteMangas = await Promise.all(favoriteMangasPromises);
-
-        return favoriteMangas;
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        throw error;
-    }
+    return favoriteMangas;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
 };
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,70 +1,72 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const { isEmail } = require('validator');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const { isEmail } = require("validator");
 
-const userSchema = mongoose.Schema({
-        username: {
-            type: String,
-            required: [true, 'Veuillez saisir un pseudo'],
-            minlength: 3,
-            maxlength: 55,
-            unique: true,
-            trim: true,
-        },
-        email: {
-            type: String,
-            required: [true, 'Veuillez saisir un email'],
-            validate: [isEmail],
-            unique: true,
-            lowercase: true,
-            trim: true,
-        },
-        password: {
-            type: String,
-            required: [true, 'Veuillez saisir un mot de passe'],
-            max: 50,
-            minlength: 6,
-        },
-        role: {
-            type: String,
-            enum: ['User', 'Admin', 'Administrateur'], // Les rôles valides
-            default: 'User' // Rôle par défaut
-          },
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Veuillez saisir un pseudo"],
+      minlength: 3,
+      maxlength: 55,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Veuillez saisir un email"],
+      validate: [isEmail],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Veuillez saisir un mot de passe"],
+      max: 50,
+      minlength: 6,
     },
 
-    {
-        timestamps: true,
-    }
-)
+    role: {
+      type: String,
+      enum: ["User", "Admin", "Administrateur"], // Les rôles valides
+      default: "User", // Rôle par défaut
+    },
+  },
+
+  {
+    timestamps: true,
+  }
+);
 // function crypte le password avant le save register
-userSchema.pre("save", async function(next) {
-    const salt = await bcrypt.genSalt(); 
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 // Function décrypte le password de l'utilisateur quand login
-userSchema.statics.login = async function(email, password) { 
-    const user = await this.findOne({ email });
-    
-    if (user) {
-        const isPasswordMatch = await bcrypt.compare(password, user.password); 
-        return isPasswordMatch; 
-    }
-    return false;
+userSchema.statics.login = async function (email, password) {
+  const user = await this.findOne({ email });
+
+  if (user) {
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    return isPasswordMatch;
+  }
+  return false;
 };
 // Function décrypte le password et modifie le password quand l'user est logué
-userSchema.methods.changePassword = async function(newPassword) {
-  this.password = newPassword; 
+userSchema.methods.changePassword = async function (newPassword) {
+  this.password = newPassword;
 
-    try {
-        await this.save();
-        return true; 
-    } catch (error) {
-        console.error('Error changing password:', error);
-        return false; 
-    }
+  try {
+    await this.save();
+    return true;
+  } catch (error) {
+    console.error("Error changing password:", error);
+    return false;
+  }
 };
 
-const User = mongoose.model('user', userSchema)
+const User = mongoose.model("user", userSchema);
 
 module.exports = User;
