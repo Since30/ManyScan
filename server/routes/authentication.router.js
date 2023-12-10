1b8296d44f06f1629c95a13afeccc2a2a5eb83b6
Bastardrosset <thexav01@outlook.fr>
2023-12-10 19:43:26 +0100

    Pull main mise a jour de dev-xav avec fonction notification forgot password && newPassword

--- server/routes/authentication.router.js
+++ server/routes/authentication.router.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const authCtrl = require('../controllers/authentication.controller');
 const jwtMiddleware = require('../config/jwt.middleware.js');
-const { validateUserRegistration, validateUserSignIn } = require('../config/sanitize.middleware.js')
+const { validateUserRegistration, validateUserSignIn } = require('../config/sanitize.middleware.js');
 
 //----- Route création de compte -----//
 router.post('/register', validateUserRegistration, authCtrl.register);
