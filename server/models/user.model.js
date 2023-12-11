f96f4867a29836f9d90ddd0d6be539b089b62e01
Steve Basse <84519539+iSteveB@users.noreply.github.com>
2023-12-11 10:36:53 +0100

    Refactor user model schema

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -3,39 +3,44 @@ const bcrypt = require('bcrypt');
 const { isEmail } = require('validator');
 
 const userSchema = mongoose.Schema({
-        username: {
-            type: String,
-            required: [true, 'Veuillez saisir un pseudo'],
-            minlength: 3,
-            maxlength: 55,
-            unique: true,
-            trim: true,
-        },
-        email: {
-            type: String,
-            required: [true, 'Veuillez saisir un email'],
-            validate: [isEmail],
-            unique: true,
-            lowercase: true,
-            trim: true,
-        },
-        password: {
-            type: String,
-            required: [true, 'Veuillez saisir un mot de passe'],
-            max: 50,
-            minlength: 6,
-        },
-        role: {
-            type: String,
-            enum: ['User', 'Admin', 'Administrateur'], // Les rôles valides
-            default: 'User' // Rôle par défaut
-          },
+    username: {
+        type: String,
+        required: [true, 'Veuillez saisir un pseudo'],
+        minlength: 3,
+        maxlength: 55,
+        unique: true,
+        trim: true,
     },
-
-    {
-        timestamps: true,
+    email: {
+        type: String,
+        required: [true, 'Veuillez saisir un email'],
+        validate: [isEmail],
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: [true, 'Veuillez saisir un mot de passe'],
+        max: 50,
+        minlength: 6,
+    },
+    role: {
+        type: String,
+        enum: ['User', 'Admin', 'Administrateur'], // Les rôles valides
+        default: 'User', // Rôle par défaut
+    },
+    refreshTokens: [{ type: String }],
+    resetToken: {
+        type: String,
+        default: null,
+    },
+    resetTokenExpiration: {
+        type: Date,
+        default: null,
     }
-)
+});
+
 // function crypte le password avant le save register
 userSchema.pre("save", async function(next) {
     const salt = await bcrypt.genSalt(); 
@@ -45,12 +50,22 @@ userSchema.pre("save", async function(next) {
 // Function décrypte le password de l'utilisateur quand login
 userSchema.statics.login = async function(email, password) { 
     const user = await this.findOne({ email });
-    
-    if (user) {
-        const isPasswordMatch = await bcrypt.compare(password, user.password); 
-        return isPasswordMatch; 
+
+    if (!user) {
+        return false;
     }
-    return false;
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+        return false;
+    }
+
+    // Génération du refresh token et ajout à la liste des refresh tokens
+    const refreshToken = generateRefreshToken();
+    user.refreshTokens.push(refreshToken);
+    await user.save();
+
+    return true;
 };
 // Function décrypte le password et modifie le password quand l'user est logué
 userSchema.methods.changePassword = async function(newPassword) {
@@ -58,10 +73,10 @@ userSchema.methods.changePassword = async function(newPassword) {
 
     try {
         await this.save();
-        return true; 
+        return true;
     } catch (error) {
         console.error('Error changing password:', error);
-        return false; 
+        return false;
     }
 };
 
