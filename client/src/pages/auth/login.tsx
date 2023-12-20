f1526a1e734be78264761f6cce6e8f0daabea246
Bastardrosset <thexav01@outlook.fr>
2023-12-20 14:59:18 +0100

    Refacto forgotPassowrd to modal

--- client/src/pages/auth/login.tsx
+++ client/src/pages/auth/login.tsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
 import { useRouter } from 'next/router'
-import { useAuth } from './authContext'
+import { useAuth } from './authContext';
+import ForgotPasswordModal from "./ForgotPasswordModal";
+
 
 export default function LoginForm() {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const { login } = useAuth()
+    const { login } = useAuth();
 
-    const router = useRouter()
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [showModal, setShowModal] = useState(false);
+    const [message, setMessage] = useState('');
+
+    const router = useRouter();
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
@@ -41,11 +46,20 @@ export default function LoginForm() {
         } catch (error) {
             console.error('Erreur lors de la connexion:', error)
         }
-    }
+    };
 
     const handleBack = () => {
         router.push('/') // Redirection vers la page d'accueil
-    }
+    };
+
+    const handleForgotPassword = () => {
+        setShowModal(true);
+    };
+    
+    const closeModal = () => {
+        setShowModal(false)
+        setMessage('')
+    };
 
     return (
         <div className='flex justify-center items-center h-screen bg-gray-100'>
@@ -86,10 +100,7 @@ export default function LoginForm() {
                             className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5'
                             required
                         />
-
                     </div>
-
-
                     <div className='mb-6'>
                         <label htmlFor='terms' className='inline-flex items-center'>
                             <input
@@ -109,7 +120,6 @@ export default function LoginForm() {
                         className='text-light bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
                         Connexion
                     </button>
-
                     <div className='mt-4'>
                         <span className='text-sm font-medium text-gray-900 dark:text-light'>
                             Vous n'avez pas de compte ?
@@ -120,18 +130,23 @@ export default function LoginForm() {
                             S'inscrire
                         </a>
                     </div>
-
                     <div className='mt-4'>
                         <a
-                            href='/auth/forgotPassword'
+                            onClick={handleForgotPassword}
                             className='text-sm font-medium text-blue-700 dark:text-blue-400'>
                             Mot de passe oublié ?
                         </a>
+                        {showModal && (
+                            <div className='fixed top-0 left-0 w-full h-full flex justify-center items-center bg-gray-900 bg-opacity-50'>
+                                <div className='bg-white p-6 rounded-lg'>
+                                    <ForgotPasswordModal closeModal={closeModal} />
+                                </div>
+                                <button onClick={closeModal}>Fermer</button>
+                            </div>
+                        )}
                     </div>
                 </form>
-
             </div>
         </div>
-
     )
 }
